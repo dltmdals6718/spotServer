f4ba216f25386d45a1be5211e677da5024b2df7a
LSM <dltmdals6718@naver.com>
2024-03-15 16:42:02 +0900

    필요없는 코드 정리

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -111,9 +111,3 @@ CREATE TABLE comment_like
     FOREIGN KEY (member_id) REFERENCES member (id),
     FOREIGN KEY (comment_id) REFERENCES comment (id)
 );
-
-INSERT INTO location(latitude, longitude, title, address, description)
-VALUES (35.24308, 128.6934, '창원대학교 운동장', '의창구 창원대학로 20 창원시 경상 남도', '낭만 가득 운동장');
-INSERT INTO location(latitude, longitude, title, address, description)
-VALUES (35.23296, 128.6805, '용지호수공원', '경상남도 창원시 의창구 용지동 551-4',
-        '용지호수는 경상남도 창원시 성산구 용지동에 있는 호수이다. 창원시를 대표하는 호수이며, 용지공원 안에 있다.');
