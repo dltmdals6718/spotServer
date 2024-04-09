b028b2bb46f2ea1ba5131cb60108f414e1b8582c
LSM <dltmdals6718@naver.com>
2024-04-09 12:34:38 +0900

    서버 TimeZone 설정

--- src/main/java/com/example/spotserver/SpotServerApplication.java
+++ src/main/java/com/example/spotserver/SpotServerApplication.java
@@ -3,10 +3,13 @@ package com.example.spotserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SpotServerApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(SpotServerApplication.class, args);
     }
 
